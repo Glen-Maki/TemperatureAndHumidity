0c95e5b2e10951126c86c8c48d7dab256df8dc40
KazuyaMakino <101450293+Glen-Maki@users.noreply.github.com>
2022-08-03 21:36:57 +0900

    [fix] 温湿度表示の部分をコンポーネント化

---
+++ frontend-react/src/pages/TempAndHum.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export const TempAndHum = () => {
+  const [data, setData] = useState<string[]>();
+
+  const sensorRead = async () => {
+    const uri = "http://192.168.0.15:8000";
+
+    // const temperature = String(20 + Math.floor(Math.random() * 11));
+    // const humidity = String(40 + Math.floor(Math.random() * 21));
+    // setData([temperature, humidity]);
+
+    await fetch(uri + "/data", {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setData([data.temperature, data.humidity]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  /*
+  useEffect(() => {
+    sensorRead();
+  }, []);*/
+  return (
+    <div>
+      {data ? (
+        <div>
+          <p>気温:{data[0]}C</p>
+          <p>湿度:{data[1]}%</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      <button onClick={sensorRead}>温湿度を取得する</button>
+    </div>
+  );
+};
