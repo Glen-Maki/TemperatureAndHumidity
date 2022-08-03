33b5145349cd28b39fba9aef5354cef415512231
KazuyaMakino <101450293+Glen-Maki@users.noreply.github.com>
2022-08-03 17:43:27 +0900

    [fix] テスト用コードの削除

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 export const App = () => {
   const [data, setData] = useState<string[]>();
 
   const sensorRead = async () => {
-    // const uri = "http://192.168.0.15:8000";
+    const uri = "http://192.168.0.15:8000";
 
-    const temperature = String(20 + Math.floor(Math.random() * 11));
-    const humidity = String(40 + Math.floor(Math.random() * 21));
-    setData([temperature, humidity]);
+    // ßconst temperature = String(20 + Math.floor(Math.random() * 11));
+    // const humidity = String(40 + Math.floor(Math.random() * 21));
+    // setData([temperature, humidity]);
 
-    /* await fetch(uri + "/data", {
+    await fetch(uri + "/data", {
       method: "GET",
     })
       .then((response) => {
@@ -23,11 +22,8 @@ export const App = () => {
         setData([data.temperature, data.humidity]);
       })
       .catch((err) => {
-        const temperature = String(20 + Math.floor(Math.random() * 11));
-        const humidity = String(40 + Math.floor(Math.random() * 21));
-        setData([temperature, humidity]);
         console.error(err);
-      });*/
+      });
   };
   /*
   useEffect(() => {
